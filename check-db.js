611a98490ac399f0c8c7d9d7cf16a7983b580bdf
unknown <codewithanonymousamit@gmail.com>
2025-08-19 21:43:48 +0530

    Remove all files to clean repository

--- check-db.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db');
-
-// Check if there are any snaps in the database
-const allSnaps = db.getAllSnaps();
-console.log('All snaps in database:', allSnaps);
-
-// Check if there are any users
-const allUsers = db.prepare('SELECT * FROM users').all();
-console.log('All users in database:', allUsers);
